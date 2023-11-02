d5a1279a71a25661e1d7f3fca7a025cf68529cde
Alessio Manieri <mani.alexander03@gmail.com>
2023-11-02 18:08:47 +0100

    ok

--- src/main/java/gioco/progettospacca/HomeController.java
+++ src/main/java/gioco/progettospacca/HomeController.java
@@ -4,7 +4,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class Controller {
+public class HomeController {
     @FXML
     Label scritta;
     public void clicca(ActionEvent actionEvent) {
