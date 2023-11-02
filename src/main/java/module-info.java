85ae04d8a920f76759109175fc90e9d7b677393b
asdru22 <76167257+asdru22@users.noreply.github.com>
2023-11-02 17:56:33 +0100

    succo di whatsapp

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,8 +2,9 @@ module gioco.progettospacca {
     requires javafx.controls;
     requires javafx.fxml;
 
-    requires org.controlsfx.controls;
+    //requires org.controlsfx.controls;
 
+    requires com.google.gson;
     opens gioco.progettospacca to javafx.fxml;
     exports gioco.progettospacca;
 }
