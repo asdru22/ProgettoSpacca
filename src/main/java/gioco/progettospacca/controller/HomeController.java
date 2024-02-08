0cfd3be9f394f56cfb1e2e0094aeeea102b03a21
Alessio Manieri <mani.alexander03@gmail.com>
2024-02-08 20:23:41 +0100

    robe(da sistemare)

--- src/main/java/gioco/progettospacca/controller/HomeController.java
+++ src/main/java/gioco/progettospacca/controller/HomeController.java
@@ -28,9 +28,37 @@ public class HomeController {
 
     }
 
+
     public void giocaTorneo(ActionEvent actionEvent) {
     }
 
     public void creaTorneo(ActionEvent actionEvent) {
     }
+
+
+    public void LoginAdmin(ActionEvent actionEvent) throws IOException
+    {
+        //aggiungere usrrname e password
+        //if(user=="admin" && psw=="spacca") {
+            Parent root = FXMLLoader.load(getClass().getResource("CreaPartita.fxml"));
+            Scene scene = new Scene(root);
+            Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+            stage.setTitle("Login amministratore");
+            stage.setScene(scene);
+            stage.show();
+        //}else
+        //{
+
+        //}
+    }
+
+    //public void BackToHome(ActionEvent actionEvent) throws IOException
+    //{
+    //   Parent root = FXMLLoader.load(getClass().getResource("HomeView.fxml"));
+    //   Scene scene = new Scene(root);
+    //  Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+    //    stage.setTitle("Home");
+    //   stage.setScene(scene);
+    //    stage.show();
+    //}
 }
