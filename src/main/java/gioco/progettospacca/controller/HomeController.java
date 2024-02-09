302b40051cdb60147803abc70025f9c1e90b72ec
Alessio Manieri <mani.alexander03@gmail.com>
2024-02-09 15:24:12 +0100

    Login Admin

--- src/main/java/gioco/progettospacca/controller/HomeController.java
+++ src/main/java/gioco/progettospacca/controller/HomeController.java
@@ -9,6 +9,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -42,20 +43,36 @@ public class HomeController {
     public void creaTorneo(ActionEvent actionEvent) {
     }
 
-
+    @FXML
+    private TextField txt_user;
+    @FXML
+    private TextField txt_psw;
+    @FXML
+    private Button btn_login;
     public void LoginAdmin(ActionEvent actionEvent) throws IOException
     {
-        //aggiungere usrrname e password
-        //if(user=="admin" && psw=="spacca") {
-            Parent root = FXMLLoader.load(getClass().getResource("CreaPartita.fxml"));
-            Scene scene = new Scene(root);
-            Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
-            stage.setTitle("Crea partita");
-            stage.setScene(scene);
-            stage.show();
-        //}else
-        //{
-        //}
+        try {
+            String user= String.valueOf((txt_user.getText()));
+            String psw= String.valueOf((txt_psw.getText()));
+            //aggiungere usrrname e password
+            if ("admin".equals(user) && "spacca".equals(psw)) {
+                Parent root = FXMLLoader.load(getClass().getResource("CreaPartita.fxml"));
+                Scene scene = new Scene(root);
+                Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+                stage.setTitle("Crea partita");
+                stage.setScene(scene);
+                stage.show();
+            } else
+            {
+                System.out.println(user);
+                System.out.println(psw);
+                System.out.println("Email o password errata");
+            }
+
+        }catch(Exception e)
+        {
+            System.out.println(e);
+        }
     }
 
     public void BackToHome(ActionEvent actionEvent) throws IOException
