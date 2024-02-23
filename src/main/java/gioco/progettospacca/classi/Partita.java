12cb7beec8eec79c430cdb7fac9c2a6e539a3d84
Daniele1103 <141152094+Daniele1103@users.noreply.github.com>
2024-02-23 13:31:29 +0100

    c

--- src/main/java/gioco/progettospacca/classi/Partita.java
+++ src/main/java/gioco/progettospacca/classi/Partita.java
@@ -199,7 +199,7 @@ public class Partita {
                                 } else {
                                     ArrayList<Integer> numeri = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
                                     Collections.shuffle(numeri);
-                                    pos = numeri.remove(0);
+                                    pos = numeri.remove(1);
                                 }
                                 if (pos >= 1 && pos <= 5) toccaA.scarta(pos - 1);
                                 else System.out.println("devi scegliere un numero tra 0 e 4");
@@ -214,7 +214,7 @@ public class Partita {
                                 } else {
                                     ArrayList<Integer> numeri = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
                                     Collections.shuffle(numeri);
-                                    pos = numeri.remove(0);
+                                    pos = numeri.remove(1);
                                 }
                                 if (pos >= 1 && pos <= 5) {
                                     toccaA.scarta(pos - i);
@@ -231,7 +231,7 @@ public class Partita {
                                 } else {
                                     ArrayList<Integer> numeri = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
                                     Collections.shuffle(numeri);
-                                    pos = numeri.remove(0);
+                                    pos = numeri.remove(1);
                                 }
                                 if (pos >= 1 && pos <= 5) {
                                     toccaA.scarta(pos - j);
