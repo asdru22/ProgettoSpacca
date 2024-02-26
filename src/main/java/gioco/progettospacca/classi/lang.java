975917e0eb3d35888998d27c2e6df597058f2aa5
asdru22 <76167257+asdru22@users.noreply.github.com>
2024-02-26 22:52:45 +0100

    traduzioni (inizio)

---
+++ src/main/java/gioco/progettospacca/classi/lang.java
@@ -0,0 +1,14 @@
+package gioco.progettospacca.classi;
+
+import java.text.MessageFormat;
+import java.util.Locale;
+
+public class lang {
+    public static void main(String[] args) {
+
+        String s = Utili.traduci("prova", Locale.ITALIAN);
+        String f = MessageFormat.format(s, "paolo");
+        System.out.println(f);
+
+    }
+}
