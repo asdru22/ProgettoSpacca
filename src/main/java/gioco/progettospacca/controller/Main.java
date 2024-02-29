0997daa2c87bee3371b2a903e27756a2f590d2e8
asdru22 <76167257+asdru22@users.noreply.github.com>
2024-02-29 10:29:57 +0100

    trad inglese

--- src/main/java/gioco/progettospacca/controller/Main.java
+++ src/main/java/gioco/progettospacca/controller/Main.java
@@ -27,7 +27,7 @@ public class Main extends Application {
     }
 
     public static void main(String[] args) {
-        Utili.setLingua(Locale.ENGLISH); // lingua di default
+        Utili.setLingua(Locale.ITALIAN); // lingua di default
         launch(args);
     }
 }
