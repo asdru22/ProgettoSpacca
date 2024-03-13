b8dca3ea233a5a319948e2fec2ea2fd0193706d7
Alessio Manieri <mani.alexander03@gmail.com>
2024-03-13 16:10:02 +0100

    prove di interfaccia partita

--- src/main/java/gioco/progettospacca/controller/PartitaController.java
+++ src/main/java/gioco/progettospacca/controller/PartitaController.java
@@ -541,7 +541,7 @@ public class PartitaController implements Initializable {
 
     public void initialize(URL url, ResourceBundle resourceBundle) {
         System.out.println(">>> initializa");
-        int codice = 59172;
+        int codice = 87280;
         p = Partita.carica(codice);
         mostraClassifica();
         cont = p.getCont();
