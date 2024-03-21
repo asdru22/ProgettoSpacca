4beb61ac0b29cf6f7067fefebdde2cc154d57a96
Alessio Manieri <mani.alexander03@gmail.com>
2024-03-21 11:19:21 +0100

    aggiornamenti privilegi

--- src/main/java/gioco/progettospacca/controller/PrivilegiAdminController.java
+++ src/main/java/gioco/progettospacca/controller/PrivilegiAdminController.java
@@ -1,4 +1,41 @@
 package gioco.progettospacca.controller;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+import static gioco.progettospacca.controller.Main.OPZ;
+
 public class PrivilegiAdminController {
+
+    @FXML
+    private Button btn_back;
+
+
+    public void BackToHome() throws IOException {
+        OPZ.premiBottone();
+        Parent root = FXMLLoader.load(getClass().getResource("HomeView.fxml"));
+
+        // Ottieni la finestra corrente
+        Stage currentStage = (Stage) btn_back.getScene().getWindow();
+
+        // Ottieni la scena corrente
+        Scene currentScene = currentStage.getScene();
+
+        // Imposta la nuova radice della scena
+        currentScene.setRoot(root);
+
+        // Imposta il titolo della finestra
+        currentStage.setTitle(OPZ.traduci("spacca"));
+    }
+
+    public void backToHome(MouseEvent mouseEvent) throws IOException {
+        BackToHome();
+    }
 }
