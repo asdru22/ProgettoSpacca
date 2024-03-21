c7728bae6d4e8cab309fafcd824a8a5d1f19b0c7
asdru22 <76167257+asdru22@users.noreply.github.com>
2024-03-21 19:38:17 +0100

    Update Torneo.java

--- src/main/java/gioco/progettospacca/classi/Torneo.java
+++ src/main/java/gioco/progettospacca/classi/Torneo.java
@@ -36,24 +36,20 @@ public class Torneo {
         return partite;
     }
 
-    public void iniziaMain() throws FileNotFoundException {
+    public void iniziaMain(int giocatori_scelti) throws FileNotFoundException {
         int bot_esistenti = 0;
         for (Giocatore g : giocatori) {
             if (g.isBot()) bot_esistenti += 1;
         }
-        Object[] o = numeroGiocatoriGiusti(giocatori.size());
-        if ((boolean) o[0]) {
-            inizia();
-        } else if ((int) o[1] >= 1) {
-            int n_bot = (int) o[1];
-            for (int i = 0; i < n_bot; i++) {
-                bot_esistenti += 1;
-                giocatori.add(new Giocatore("bot" + bot_esistenti, true));
-            }
-            System.out.println("Aggiunti " + n_bot + " bot per raggiungere il numero di giocatori richiesti (" + giocatori.size() + ")");
-            inizia();
-        } else throw new Error("Errore inizio torneo");
+        int n_bot = giocatori_scelti-giocatori.size();
+        for (int i = 0; i < n_bot; i++) {
+            bot_esistenti += 1;
+            giocatori.add(new Giocatore("bot" + bot_esistenti, true));
+        }
+        System.out.println("Aggiunti " + n_bot + " bot per raggiungere il numero di giocatori richiesti (" + giocatori.size() + ")");
+        inizia();
     }
+
 
     public void elimina() {
         Utili.elimina(id, "tornei");
@@ -119,29 +115,5 @@ public class Torneo {
             partite.add(new Partita(coppia, id));
             System.out.println("Creata nuova partita con:" + coppia[0].getNome() + " e " + coppia[1].getNome());
         }
-    }
-
-    private Object[] numeroGiocatoriGiusti(int n) {
-        Object[] r = new Object[]{false, 0};
-        int i = 1;
-        // controlla se il numero di giocatori è una potenza di 2 e dunque è possibile fare un torneo
-        while (!(boolean) r[0] && i <= MAX_ROUND) {
-            r[0] = (n == Math.pow(i, 2));
-            i += 1;
-        }
-        if (!(boolean) r[0]) {
-            r[1] = numeroBot(n);
-        }
-        return r;
-    }
-
-    private int numeroBot(int n) {
-        // Trova la potenza di 2 più grande vicina a n
-        int i = 1;
-        while (Math.pow(2, i) <= n) {
-            i++;
-        }
-        // Restituisci la potenza di 2 più grande e più vicina a n
-        return (int) Math.pow(2, i) - n;
     }
 }
