3576d74c9b747eb11af19d9c2ddfe9a33b62f28c
Alessio Manieri <mani.alexander03@gmail.com>
2024-03-23 18:54:09 +0100

    n

--- src/main/java/gioco/progettospacca/controller/CreaTorneoController.java
+++ src/main/java/gioco/progettospacca/controller/CreaTorneoController.java
@@ -1,11 +1,28 @@
 package gioco.progettospacca.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
-import java.awt.event.ActionEvent;
 import java.io.IOException;
 
+import static gioco.progettospacca.controller.Main.OPZ;
+
 public class CreaTorneoController {
+    public void BackToHome(ActionEvent actionEvent) throws IOException {
+        OPZ.premiBottone();
+        Parent root = FXMLLoader.load(getClass().getResource("HomeView.fxml"));
+        Scene scene = new Scene(root);
+        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        stage.setTitle(OPZ.traduci("spacca"));
+        stage.setScene(scene);
+        scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
+        stage.show();
+    }
 
 }
--- src/main/java/gioco/progettospacca/controller/PartiteSalvateController.java
+++ src/main/java/gioco/progettospacca/controller/PartiteSalvateController.java
@@ -1,4 +1,21 @@
 package gioco.progettospacca.controller;
 
+import javafx.fxml.FXML;
+
+import javax.swing.text.html.ListView;
+import java.io.File;
+
 public class PartiteSalvateController {
+    @FXML
+    private ListView list_listaPartite;
+    /*
+    File folder = new File("src/main/java/gioco/progettospacca/salvataggi/partite");
+    File[] file_partite = folder.listFiles();
+    for( File file : file_partite)
+    {
+        if (file.isFile()) {
+            String s = file.getName().substring(0, file.getName().length() - 5);
+        }
+    }
+    */
 }
