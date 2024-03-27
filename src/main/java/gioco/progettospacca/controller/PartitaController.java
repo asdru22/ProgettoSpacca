e65328f989ce2cec82c668d1bd3529f8de2c033f
Alessio Manieri <mani.alexander03@gmail.com>
2024-03-27 13:31:24 +0100

    schermata vittoria

--- src/main/java/gioco/progettospacca/controller/PartitaController.java
+++ src/main/java/gioco/progettospacca/controller/PartitaController.java
@@ -35,7 +35,7 @@ import java.util.*;
 import static gioco.progettospacca.controller.Main.OPZ;
 
 public class PartitaController implements Initializable {
-    public static final int CODICE_TEMP = 74564;
+    public static final int CODICE_TEMP = 25120;
     @FXML
     public Label lbl_pausa;
 
