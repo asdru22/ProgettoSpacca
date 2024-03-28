e6c23c6be9be763b1fe25f6f6532efc74a140336
Alessio Manieri <mani.alexander03@gmail.com>
2024-03-28 12:03:39 +0100

    gioca torneo

--- src/main/java/gioco/progettospacca/controller/GiocaTorneoController.java
+++ src/main/java/gioco/progettospacca/controller/GiocaTorneoController.java
@@ -6,6 +6,8 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 import java.awt.*;
@@ -14,6 +16,8 @@ import java.io.IOException;
 import static gioco.progettospacca.controller.Main.OPZ;
 
 public class GiocaTorneoController {
+    @FXML
+    private Button btn_entraTorneo;
 
     public void BackToHome(ActionEvent actionEvent) throws IOException {
         OPZ.premiBottone();
@@ -25,4 +29,29 @@ public class GiocaTorneoController {
         scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
         stage.show();
     }
+    public void giocaTorneo(MouseEvent mouseEvent) throws IOException {
+        OPZ.premiBottone();
+        EventoGiocaTorneo();
+    }
+
+    public void EventoGiocaTorneo() throws IOException
+    {
+
+        Parent root = FXMLLoader.load(getClass().getResource("TorneoView.fxml"));
+
+        // Ottieni la finestra corrente
+        Stage currentStage = (Stage) btn_entraTorneo.getScene().getWindow();
+
+        // Ottieni la scena corrente
+        Scene currentScene = currentStage.getScene();
+
+        // Imposta la nuova radice della scena
+        currentScene.setRoot(root);
+
+        // Imposta il titolo della finestra
+        currentStage.setTitle(OPZ.traduci("gioca_torneo"));
+
+
+    }
+
 }
