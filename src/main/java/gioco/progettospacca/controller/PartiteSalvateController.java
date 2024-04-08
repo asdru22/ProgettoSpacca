520b39846deee4b012b098d5ee795161e4520a2a
asdru22 <76167257+asdru22@users.noreply.github.com>
2024-04-08 11:11:56 +0200

    traduzioni elimina

--- src/main/java/gioco/progettospacca/controller/PartiteSalvateController.java
+++ src/main/java/gioco/progettospacca/controller/PartiteSalvateController.java
@@ -1,13 +1,32 @@
 package gioco.progettospacca.controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 
-import javax.swing.text.html.ListView;
 import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class PartiteSalvateController {
+import static gioco.progettospacca.controller.Main.OPZ;
+
+public class PartiteSalvateController implements Initializable {
     @FXML
-    ListView list_listaPartite;
+    Button btn_eliminaPartita,btn_eliminaTorneo;
+    @FXML
+    Label lbl_titoloPartite, lbl_titoloTornei;
+    @FXML
+    ListView list_partita,list_listaTornei;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        btn_eliminaPartita.setText(OPZ.traduci("elimina_partita"));
+        btn_eliminaTorneo.setText(OPZ.traduci("elimina_torneo"));
+        lbl_titoloPartite.setText(OPZ.traduci("partite"));
+        lbl_titoloTornei.setText(OPZ.traduci("tornei"));
+    }
     /*
     File folder = new File("src/main/java/gioco/progettospacca/salvataggi/partite");
     File[] file_partite = folder.listFiles();
