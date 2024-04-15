9fe8b99b995e359c272264b4f2f6cae1b62aad10
asdru22 <76167257+asdru22@users.noreply.github.com>
2024-04-15 10:42:58 +0200

    traduzione regole

--- src/main/java/gioco/progettospacca/controller/RegoleController.java
+++ src/main/java/gioco/progettospacca/controller/RegoleController.java
@@ -10,10 +10,11 @@ import java.util.ResourceBundle;
 import static gioco.progettospacca.controller.Main.OPZ;
 
 public class RegoleController implements Initializable {
-    @FXML Label lbl_regolamento;
+    @FXML Label lbl_regolamento,lbl_regole;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         lbl_regolamento.setText(OPZ.traduci("regolamento"));
+        lbl_regole.setText(OPZ.traduci("regole1"));
     }
 }
