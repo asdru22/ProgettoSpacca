c0d8d7842ce58b35d698d38a3074b39b43d51763
Daniele1103 <141152094+Daniele1103@users.noreply.github.com>
2024-04-16 01:14:45 +0200

    c

--- src/main/java/gioco/progettospacca/classi/Mazzo.java
+++ src/main/java/gioco/progettospacca/classi/Mazzo.java
@@ -21,7 +21,7 @@ public class Mazzo {
     public static ArrayList<Carta> creaMazzoIniziale() {
         Seme[] semi = Seme.values();
         ArrayList<Carta> temp = new ArrayList<>();
-        //temp.add(new Carta(semi[5],15, "/gioco/progettospacca/carte/imprevisti/1.png"));
+        temp.add(new Carta(semi[5],15, "/gioco/progettospacca/carte/imprevisti/1.png"));
         temp.add(new Carta(semi[5],15, "/gioco/progettospacca/carte/imprevisti/2.png"));
         for (int i = 0; i < 10; i++) {
             temp.add(new Carta(semi[0], i, "/gioco/progettospacca/carte/fuoco/" + (i + 1) + ".png"));
@@ -31,7 +31,7 @@ public class Mazzo {
             temp.add(new Carta(semi[4], i, "/gioco/progettospacca/carte/acqua/" + (i + 1) + ".png"));
         }
 
-        //Collections.shuffle(temp);
+        Collections.shuffle(temp);
         return temp;
     }
 
